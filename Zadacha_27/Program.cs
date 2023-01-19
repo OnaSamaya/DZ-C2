48cfceb8418c784265e44e3a760947a878f45999
Marina <demina-marina_87@list.ru>
2023-01-19 17:36:14 +0300

    commits  Please enter the commit message for your changes. Lines starting

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите целое число ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (num > 9)
+{
+    Console.WriteLine($"Сумма цифр введенного числа {num} равна {Sum(num)}");
+}
+else
+{
+    Console.WriteLine($"Введено однозначное число {num}");
+}
+int Sum(int number)
+{
+    int add = 0;
+    int temp = 0;
+    while (number > 0)
+    {
+        temp = number % 10;
+        add = add + temp;
+        number = number / 10;
+    }
+    return add;
+}
